a573e873b21983b5864b38267c98e0523045ec0f
Adam Garcia <adgar1523@gmail.com>
2023-02-05 21:18:42 -0700

    scrollspy

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
-import intersectObserve from './intersection.js';
-import ScrollSpy from 'scrollspy-js';
-
-var spy = new ScrollSpy('#js-scrollspy', {
-    nav: '.js-scrollspy-nav > li > a',
-    className: 'is-inview'
-  });
+const portfolioSections = document.querySelectorAll('.portfolio-section');
+const helloSection = document.querySelector('#hello');
+const helloSectionParagraph = document.querySelector('#hello p');
+const helloSectionHeader = document.querySelector('#hello h2');
+const navLinks = document.querySelectorAll('#footer-nav a');
 
 
-// intersectObserve();
+const addActiveClass = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log(entry.target);
+        }
+    })
+}
 
+const options = {};
+const observer = new IntersectionObserver(addActiveClass, options); 
+
+portfolioSections.forEach(section => {
+    observer.observe(section);
+})
--- js/intersection.js
+++ js/intersection.js
@@ -1,18 +1,22 @@
+const portfolioSections = document.querySelectorAll('.portfolio-section');
 const helloSection = document.querySelector('#hello');
 const helloSectionParagraph = document.querySelector('#hello p');
 const helloSectionHeader = document.querySelector('#hello h2');
+const navLinks = document.querySelectorAll('#footer-nav a');
 
 export default function intersectObserve() {
     // Create an IntersectionObserver
     var observer = new IntersectionObserver(function (entries) {
-        // Check if the element is intersecting
-        if (entries[0].isIntersecting) {
-            // Add the class to the element to trigger the animation
-            helloSectionHeader.classList.add("smooth-appear");
-            helloSectionParagraph.classList.add("come-from-right")
-            // Stop observing the element
-            observer.unobserve(helloSection);
-        }
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // Add the class to the element to trigger the animation
+                helloSectionHeader.classList.add("smooth-appear");
+                // helloSectionParagraph.classList.add("come-from-right")
+
+                // Stop observing the element
+                observer.unobserve(helloSection);
+            }
+        })
     });
     
     // Start observing the element
