95c316a9168ba68087655b5b1fac067626399be7
Adam Garcia <adgar1523@gmail.com>
2023-02-06 07:23:47 -0700

    successful scrollspy

---
+++ README.md
@@ -0,0 +1,2 @@
+#Useful Links
+https://dev.to/areeburrub/change-nav-link-s-style-as-you-scroll-4p62
--- js/app.js
+++ js/app.js
@@ -2,18 +2,28 @@ const portfolioSections = document.querySelectorAll('.portfolio-section');
 const helloSection = document.querySelector('#hello');
 const helloSectionParagraph = document.querySelector('#hello p');
 const helloSectionHeader = document.querySelector('#hello h2');
-const navLinks = document.querySelectorAll('#footer-nav a');
+const navLinks = document.querySelectorAll('#footer-nav li');
 
 
-const addActiveClass = (entries, observer) => {
+const addActiveClass = (entries, options) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            console.log(entry.target);
+            const current = entry.target.getAttribute('id');
+
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.classList.contains(current)) {
+                    link.classList.add('active');
+                }
+            })
         }
     })
 }
 
-const options = {};
+const options = {
+    root: null,
+    threshold: .5 // observe halfway through section
+};
 const observer = new IntersectionObserver(addActiveClass, options); 
 
 portfolioSections.forEach(section => {
