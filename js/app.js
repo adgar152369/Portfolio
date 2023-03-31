4e0df0ec6701ce147f4f8a4546313f60b5c2621f
Adam Garcia <adgar1523@gmail.com>
2023-03-30 21:01:00 -0600

    add project

--- js/app.js
+++ js/app.js
@@ -6,31 +6,45 @@ const helloSectionParagraph = document.querySelector('#hello p');
 const helloSectionHeader = document.querySelector('#hello h2');
 const navLinks = document.querySelectorAll('#footer-nav li');
 
+const carouselImages = document.querySelector('.project-carousel-images');
+const thumbnailImages = document.querySelectorAll('.project-carousel-thumbnails img');
+
+thumbnailImages.forEach(thumbnail => {
+  thumbnail.addEventListener('click', () => {
+    const index = thumbnail.dataset.index;
+    const translateX = -index * carouselImages.clientWidth;
+    carouselImages.style.transform = `translateX(${translateX}px)`;
+
+    thumbnailImages.forEach(thumbnail => thumbnail.classList.remove('active'));
+    thumbnail.classList.add('active');
+  });
+});
+
 
 const addActiveClass = (entries, options) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            const current = entry.target.getAttribute('id');
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			const current = entry.target.getAttribute('id');
 
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                if (link.classList.contains(current)) {
-                    link.classList.add('active');
-                }
-            })
-        }
-    })
+			navLinks.forEach(link => {
+				link.classList.remove('active');
+				if (link.classList.contains(current)) {
+					link.classList.add('active');
+				}
+			})
+		}
+	})
 }
 
 const options = {
-    root: null,
-    threshold: .5 // observe halfway through section
+	root: null,
+	threshold: .5 // observe halfway through section
 };
-const observer = new IntersectionObserver(addActiveClass, options); 
+const observer = new IntersectionObserver(addActiveClass, options);
 
 portfolioSections.forEach(section => {
-    observer.observe(section);
-})
+	observer.observe(section);
+});
 
 
 intersectObserve();
