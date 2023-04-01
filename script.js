d48076d6b1665c1f83f92ff1b61aef458921958d
Adam Garcia <gar15041@byui.edu>
2023-04-01 15:11:18 -0600

    mobile navigation bg color and image

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const homeSection = document.querySelector('#home');
 
 // Add a click event listener to the navbar burger button
 navbarBurger.addEventListener('click', () => {
-  // Toggle the 'is-active' class on both the navbar burger button and the navbar menu
   navbarBurger.classList.toggle('is-active');
   navbarMenu.classList.toggle('is-active');
 });
@@ -17,28 +16,19 @@ navbarBurger.addEventListener('click', () => {
 // Add a click event listener to all the navbar menu items
 navbarMenuItems.forEach(item => {
   item.addEventListener('click', () => {
-    // Remove the 'is-active' class from both the navbar burger button and the navbar menu
     navbarBurger.classList.remove('is-active');
     navbarMenu.classList.remove('is-active');
   });
 });
 
 
-// Intersection Observer
-
-
 // create a new intersection observer
 const observer = new IntersectionObserver(entries => {
-  // check if the about section is in view
   if (entries[0].isIntersecting) {
-    console.log('about')
-    // add a class to the navbar to change its background
     navbar.classList.remove('solid-bg');
   } else {
-    // remove the class if the about section is not in view
     navbar.classList.add('solid-bg');
   }
 });
 
-// observe the about section
 observer.observe(homeSection);
