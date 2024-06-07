2071d49cbc3b512203713e4af5c843637f8d94d0
Adam <adgar1523@gmail.com>
2024-06-07 06:37:12 -0700

    create color palette

---
+++ script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 }); // Trigger at 50% visibility
+
+sections.forEach(section => {
+  observer.observe(section);
+});
