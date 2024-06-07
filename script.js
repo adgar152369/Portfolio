dc747d336d4ffb46209f8e123fa701b7341e95f1
Adam <adgar1523@gmail.com>
2024-06-07 07:34:28 -0700

    apply open and close nav menu

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const sections = document.querySelectorAll('section');
+const navOpen = document.querySelector('.nav-open');
+const navClose = document.querySelector('.nav-close');
+const navigationMenu = document.querySelector('.main-header');
+
+navOpen.addEventListener('click', openNavigation);
+
+function openNavigation() {
+  navigationMenu.classList.add('opened');
+  navClose.addEventListener('click', () => {
+    navigationMenu.classList.add('closed');
+    navigationMenu.classList.remove('opened');
+  })
+}
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
